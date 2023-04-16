f258c541689b077de0af7fca0c55099289e37915
Newton2681 <igorka2681@gmail.com>
2023-04-16 20:08:37 +0300

    added video-fashion

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
-const { src, dest, watch, parallel, series } = require('gulp');
-const scss = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default;
-const imagemin = require('gulp-imagemin');
-const browserSync = require('browser-sync').create();
-const autoprefixer = require('gulp-autoprefixer');
-const clean = require('gulp-clean');
+const { src, dest, watch, parallel, series } = require("gulp");
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const imagemin = require("gulp-imagemin");
+const browserSync = require("browser-sync").create();
+const autoprefixer = require("gulp-autoprefixer");
+const clean = require("gulp-clean");
 
 function styles() {
-  return src('app/scss/style.scss')
+  return src("app/scss/style.scss")
     .pipe(
       autoprefixer({
-        overrideBrowserslist: ['last 10 version'],
+        overrideBrowserslist: ["last 10 version"],
         grid: true,
       })
     )
-    .pipe(concat('style.min.css'))
+    .pipe(concat("style.min.css"))
     .pipe(
       scss({
-        outputStyle: 'compressed',
+        outputStyle: "compressed",
       })
     )
-    .pipe(dest('app/css'))
+    .pipe(dest("app/css"))
     .pipe(browserSync.stream());
 }
 
@@ -29,22 +29,23 @@ function scripts() {
   return src([
     /* 'node_modules/swiper/swiper-bundle.js', */
     /* подключение плагина слайдера */
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/slick-carousel/slick/slick.js',
-    'app/js/main.js',
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/slick-carousel/slick/slick.js",
+    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
+    "app/js/main.js",
 
     /* 'app/js/*.js',
       '!app/js/main.min.js', */
     /* уберает бесконечный цикл, если в папке js много файлов */
   ])
-    .pipe(concat('main.min.js'))
+    .pipe(concat("main.min.js"))
     .pipe(uglify())
-    .pipe(dest('app/js'))
+    .pipe(dest("app/js"))
     .pipe(browserSync.stream());
 }
 
 function images() {
-  return src('app/images/**/*.*')
+  return src("app/images/**/*.*")
     .pipe(
       imagemin([
         imagemin.gifsicle({
@@ -69,45 +70,39 @@ function images() {
         }),
       ])
     )
-    .pipe(dest('dist/images'));
+    .pipe(dest("dist/images"));
 }
 
-
 function watching() {
-  watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/**/*.html']).on('change', browserSync.reload);
+  watch(["app/scss/**/*.scss"], styles);
+  watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
+  watch(["app/**/*.html"]).on("change", browserSync.reload);
 }
 
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir: 'app/',
+      baseDir: "app/",
     },
     notify: false,
   });
 }
 
-
 function cleanDist() {
-  return src('dist').pipe(clean());
+  return src("dist").pipe(clean());
 }
-
 
 function building() {
-  return src(['app/css/style.min.css', 'app/js/main.min.js', 'app/*.html'], {
-    base: 'app',
-  }).pipe(dest('dist'))
+  return src(["app/css/style.min.css", "app/js/main.min.js", "app/*.html"], {
+    base: "app",
+  }).pipe(dest("dist"));
 }
-
-
 
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
 exports.images = images;
-
 
 exports.build = series(cleanDist, images, building);
 exports.default = parallel(styles, scripts, browsersync, watching);
